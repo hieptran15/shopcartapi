c58f722e9825de85abf2744b9575b2a4a6e02df8
hieptran15 <tranvanhiep15061998@gmail.com>
2020-10-29 14:47:45 +0700

    server new

--- server.js
+++ server.js
@@ -117,6 +117,18 @@ app.post("/api/order",async (req,res)=>{
 })
 
 app.get("/search/:title",(req,res)=>{
+     // Website you wish to allow to connect
+     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+     // Request methods you wish to allow
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+     // Request headers you wish to allow
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+     // Set to true if you need the website to include cookies in the requests sent
+     // to the API (e.g. in case you use sessions)
+     res.setHeader('Access-Control-Allow-Credentials', true);
     var regex=new RegExp(req.params.title,"i");
     Product.find({title:regex}).then((result)=>{
         res.status(200).json(result)
